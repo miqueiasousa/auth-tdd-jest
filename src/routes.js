c888f14f45c8ae82f274d07b02e0e19184a77d14
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-02-16 04:24:52 -0300

    Create Routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 const { Router } = require('express');
 
+const authMiddleware = require('./app/middleware/auth');
+
+const SessionController = require('./app/controllers/SessionController');
+
 const router = Router()
 
-// definição das rotas
+router.post('/sessions', SessionController.store)
+
+router.use(authMiddleware)
+
+router.get('/dashboard', (req, res) => res.status(200).send())
 
 module.exports = router
